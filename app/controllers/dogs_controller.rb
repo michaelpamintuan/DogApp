872328812bf1caeeaf2ac5cb364e39c273daf422
michaelpamintuan <michael@paralejasconsulting.com>
2022-12-07 18:29:37 +0800

    New dog-app

---
+++ app/controllers/dogs_controller.rb
@@ -0,0 +1,36 @@
+class DogsController < ApplicationController
+  def index
+    @dogs = Dog.all
+  end
+
+  def show
+    @dog = Dog.find(params[:id])
+  end
+
+  def new
+    @dog = Dog.new
+  end
+
+  def create
+    @dog = Dog.new(dog_params)
+
+    redirect_to dog_path(@dog)
+  end
+
+  def edit
+    @dog = Dog.find(params[:id])
+  end
+
+  def update
+    @dog = Dog.find(params[:id])
+    @dog.update(dog_params)
+
+    redirect_to dog_path(@dog)
+  end
+
+  private
+
+  def dog_params
+    params.require(:dog).permit(:name, :motto)
+  end
+end
---
+++ app/models/dog.rb
@@ -0,0 +1,4 @@
+class Dog < ApplicationRecord
+    validates :title, presence: true
+  validates :body, presence: true
+end
